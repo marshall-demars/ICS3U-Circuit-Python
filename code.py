de2f24fa256c6208349f28c05a4f69016fe9ab61
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-11-28 10:54:23 -0500

    0-11

--- code.py
+++ code.py
@@ -126,6 +126,12 @@ def game_scene():
     alien_count = 0
     score = 0
 
+    score_text = stage.Text(width=29, height=14, font=None palette=constants.RED_PALETTE, buffer=None)
+    score_text.clear()
+    score_text.cursor(0,0)
+    score_text.move(1,1)
+    score_text.text("Score: {0}".format(score))
+
     def show_alien():
         # this function takes an alien from off screen and moves it on screen
         for alien_number in range(len(aliens)):
@@ -193,10 +199,8 @@ def game_scene():
     # create a stage for the background to show up on
     #  and set the frame rate to 60 fps
     game = stage.Stage(ugame.display, constants.FPS)
-
     # set the layers of all sprites, items show up in order
-    game.layers = aliens + lasers + [ship] + [background]
-
+    game.layers = [score_text] + aliens + lasers + [ship] + [background]
     # render all sprites
     game.render_block()
 
@@ -267,6 +271,11 @@ def game_scene():
                         constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
                     )
                     show_alien()
+                    score -= 1
+                    score_text.clear()
+                    score_text.cursor(0,0)
+                    score_text.move(1,1)
+                    score_text.text("Score: {0}".format(score))
 
         game.render_sprites(aliens + lasers + [ship])
         game.tick()
@@ -293,6 +302,12 @@ def game_scene():
                             lasers[laser_number].move(
                                 constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
                             )
+                            # add 1 to the score
+                            score += 1
+                            score_text.clear()
+                            score_text.cursor(0,0)
+                            score_text.move(1,1)
+                            score_text.text("Score: {0}".format(score))
                             sound.stop()
                             sound.play(boom_sound)
                             show_alien()
